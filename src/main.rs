06a638b64231e8529bd707a9522ad5a08f5ad576
vuvkoblkb <bilqis8901@gmail.com>
2025-06-11 18:27:31 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ use url::Url;
 const REQUESTS_PER_SECOND: u32 = 75;
 const MAX_DEPTH: u32 = 3;
 const TIMEOUT_SECONDS: u64 = 10;
-const MAX_URLS_PER_DOMAIN: usize = 500;
+const MAX_URLS_PER_DOMAIN: usize = 1000;
 
 #[derive(Error, Debug)]
 pub enum ScannerError {
